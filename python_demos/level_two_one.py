c6b08a767b6713a97cb8e1bce8e2fd41e894fbac
Arturo Quintanilla <art9518@gmail.com>
2018-04-23 16:14:04 -0500

    Add files via upload
    Updated level_two_one.py and added level_two_one_alt.py (implementing regular expression)

--- python_demos/level_two_one.py
+++ python_demos/level_two_one.py
@@ -3,4 +3,24 @@ FIND 33:
 Given a list of integers, return True if the array contains a 3 next to a 3 somewhere.
 """
 def has_33(nums):
-	pass
+	is_33 = False
+	for i in range(len(nums)):
+		if i == 0:
+			if (nums[i] == 3 and nums[i] == nums[i+1]):
+				is_33 = True
+				break
+			else:
+				continue
+		if i == (len(nums)-1):
+			if (nums[i] == 3 and nums[i] == nums[i-1]):
+				is_33 = True
+				break
+			else:
+				continue
+		if nums[i] == 3 and (nums[i] == nums[i+1] or nums[i] == nums[i-1]):
+			is_33 = True
+	return is_33
+
+print("Test #1: {}".format(has_33([1, 3, 3])))
+print("Test #2: {}".format(has_33([1, 3, 1, 3])))
+print("Test #3: {}".format(has_33([3, 1, 3])))
